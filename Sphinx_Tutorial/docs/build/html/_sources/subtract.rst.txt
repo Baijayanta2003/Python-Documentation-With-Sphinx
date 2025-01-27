b209f7bea19d8103a43df7ccf4ea5dc1dc4c7511
Baijayanta Bhattacharyya <baijayantabhattacharyya2003@gmail.com>
2025-01-27 23:53:41 +0530

    Add files via upload

---
+++ Sphinx_Tutorial/docs/build/html/_sources/subtract.rst.txt
@@ -0,0 +1,7 @@
+subtract module
+===============
+
+.. automodule:: subtract
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ Sphinx_Tutorial/docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path if needed
+
+project = 'Documenting Python Code with Sphinux'
+copyright = '2025, Baijaynta Bhattacharyya'
+author = 'Baijaynta Bhattacharyya'
+release = "'0.0.1'"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'English'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+extensions.append("sphinx_wagtail_theme")
+html_theme = 'sphinx_wagtail_theme'
+html_static_path = ['_static']
